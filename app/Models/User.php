d3c700e896eedebc3246bbd4a4fc8989a8d38b37
VillaMarCruz <marlonvillamar.12@gmail.com>
2024-08-02 13:48:17 -0500

    feat: Table Relations ✨

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -50,8 +53,28 @@ class User extends Authenticatable
         ];
     }
 
-    public function country()
+    public function country(): BelongsTo
     {
         return $this->belongsTo(Country::class);
+    }
+
+    public function calendars(): BelongsToMany
+    {
+        return $this->belongsToMany(Calendar::class);
+    }
+
+    public function departments(): BelongsToMany
+    {
+        return $this->belongsToMany(Department::class);
+    }
+
+    public function holidays(): HasMany
+    {
+        return $this->hasMany(Holiday::class);
+    }
+
+    public function timesheets(): HasMany
+    {
+        return $this->hasMany(Timesheet::class);
     }
 }
