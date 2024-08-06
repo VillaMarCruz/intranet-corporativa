e82b1183a9d5117ffcd4cf9f97aad06c6fb1f53c
VillaMarCruz <marlonvillamar.12@gmail.com>
2024-08-06 12:18:14 -0500

    feat: Holidays Resource ✨

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,9 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'test@test.com',
         ]);
+
+        $this->call(WorldTableSeeder::class);
     }
 }
